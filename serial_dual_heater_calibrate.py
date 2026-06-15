350a0cb8d73e7dc72b6b2474473a3b47db843754
ZY_Night <94029875+monodgi@users.noreply.github.com>
2026-06-15 15:04:22 +0800

    Delete serial_dual_heater_calibrate.py

--- serial_dual_heater_calibrate.py
+++
@@ -1,8 +0,0 @@
-import importlib.util, sys, os, glob
-_d = os.path.dirname(os.path.abspath(__file__))
-_so = glob.glob(os.path.join(_d, 'serial_dual_heater_calibrate*.so'))
-if not _so: raise ImportError("serial_dual_heater_calibrate .so not found")
-_spec = importlib.util.spec_from_file_location('serial_dual_heater_calibrate_impl', _so[0])
-_mod = importlib.util.module_from_spec(_spec)
-_spec.loader.exec_module(_mod)
-sys.modules[__name__] = _mod
